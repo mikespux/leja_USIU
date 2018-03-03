04dcf2cfbe62a3bc2a91b8d7fe8cda0bb9916535
mikespux <orengemichael12@gmail.com>
2018-03-03 16:03:41 +0300

    Swahili version
    Translated leja platform

--- index.php
+++ index.php
@@ -14,10 +14,23 @@
 	$password = "";
 	$dbname="leja";
 */
+	
+
+
 	//NGROK
 	$user_phonenumber = $_POST['phoneNumber'];
     $phonenumber = str_replace("+", "", $user_phonenumber); 	//remove the "+" in phone number
-    $sessionID = $_POST['sessionId'];  
+	$sessionID = $_POST['sessionId'];  
+	
+
+
+
+
+
+
+
+
+	
     $servicecode = $_POST['serviceCode'];  
     $ussdString = $_POST['text'];
 
@@ -36,7 +49,9 @@
 	// Check connection
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
-	}
+	}else
+		//echo "Connected to database successfully <br/><br/>";
+
 
 	$level =0; 
 
@@ -49,16 +64,37 @@
     //echo ussd_text
     function ussd_proceed ($ussd_text){  
     	echo $ussd_text;  
-	}
+    	//exit(0);  
+    }
 	
+
 	//If user exists or not
     $query = "SELECT * from subscribers where phone_Number ='$phonenumber'";
 	if ($result=mysqli_query($conn,$query)){
 		if(mysqli_num_rows($result) > 0){
-			//View my account menu			
+			//View my account menu
+			
 			if ($level==0){
+
+				
 				displaymenu();
-			}    
+
+
+				/*
+				//Check if table is empty
+				$query = "SELECT * FROM $username";
+				// execute query 
+				$result = mysqli_query($conn, $query) or die ("Error in query: $query. ".mysqli_error($conn)); 
+				// see if any rows were returned 
+				if (mysqli_num_rows($result) > 0) { //===Table exist======= Some orders exist
+					displaymenu();
+				}
+				else{//====Nothing in table====== No orders
+					displayNewMemberMenu();
+				} 
+				*/
+			}  		   
+			      
 		    if ($level>0){  
 			    switch ($ussdString_explode[0]) {  
 				    case 1: //Update stock 
@@ -72,7 +108,7 @@
 				    	break; 
 				    case 4: 
 				    	//Loans 
-				   		$ussd_text="END <br>You are not legible to receive loans. You must use Leja for at least six months.\nThank you.";  
+				   		$ussd_text="END <br>Huwezi kupokea mikopo. Lazima utumie Leja kwa angalau miezi sita. Asante.";  
 			   			ussd_proceed($ussd_text);
 				    	break;
 				    case 5:
@@ -88,16 +124,16 @@
 				    	die();
 				    	break;
 				    default:
-				    	$ussd_text = "Invalid option";
+				    	$ussd_text = "Chaguo batili";
 						ussd_proceed($ussd_text);
 				    	break; 
-				}  //End switch
+				}  
 		    }  
 		}
 		else{
 		  	//Registration menu
 		  	 if ($level==0){  
-			    	$ussd_text="CON \nWelcome to LEJA.\n1. Register\n2. About Leja\n3. Choose Kiswahili";  
+			    	$ussd_text="CON \Karibu LEJA.\n1. Daftari\n2. Kuhusu Leja\n3. Kiswahili";  
 			   		ussd_proceed($ussd_text);    
 			    } 
 				
@@ -117,7 +153,7 @@
 					    	require("lejaSwahili.php");
 					    	die();
 				    		break;
-				    }//End switch  
+				    }  
 			    }  	
 			}//End else
 		}
@@ -126,13 +162,14 @@
 			ussd_proceed($ussd_text);
 		}
 
+
 	function displaymenu(){  
-		$ussd_text="CON \n1: Update Purchases\n2: Update Sales\n3: Profits and Losses \n4: Loans \n5: Statements \n 6: Help \n00: Exit";  
+		$ussd_text="CON \n1: Sasisha Ununuzi\n2: Sasisha Mauzo\n3: Faida na Hutoa \n4: Mikopo \n5: Taarifa na Taarifa fupi \n 6: Usaidizi \n00: Toka";  
 		ussd_proceed($ussd_text);  
 	}
 
 	function displayNewMemberMenu(){
-		$ussd_text = "CON \nWe are happy to see you here.<br>1. Create new order <br>00. Exit";
+		$ussd_text = "CON \nTunafurahi kukuona hapa.<br>1. Unda stoke mpya <br>00. Exit";
 		ussd_proceed($ussd_text);
 	}
 
@@ -152,27 +189,27 @@
 
 	function register($details,$phone, $conne){      
 		if (count($details)==1){  
-			$ussd_text="CON \n Enter your first name:";  
+			$ussd_text="CON \n Weka jina la kwanza:";  
 			ussd_proceed($ussd_text);  
 		} 
 		else if(count($details) == 2){		  
-			$ussd_text = "CON Enter your last name:\n";  
+			$ussd_text = "CON Weka jina la mwisho:\n";  
 			ussd_proceed($ussd_text);
 		}
 		else if(count($details) == 3){		  
-			$ussd_text = "CON Enter your National ID number:\n";  
+			$ussd_text = "CON Weka nambari yako ya kitambulisho:\n";  
 			ussd_proceed($ussd_text);  
 		}
 		else if(count($details) == 4){
-			$ussd_text = "CON <br> Enter your email address";
+			$ussd_text = "CON <br> Weka anwani ya barua pepe";
 			ussd_proceed($ussd_text);
 		}
 		else if(count($details) == 5){		  
-			$ussd_text = "CON \n Enter your type of business:\n";  
+			$ussd_text = "CON \n Weka aina ya biashara:\n";  
 			ussd_proceed($ussd_text);  
 		}
 		else if(count($details) == 6){		  
-			$ussd_text = "CON \n Reply with your residential county:\n";  
+			$ussd_text = "CON \n Jibu na kata yako ya makazi:\n";  
 			ussd_proceed($ussd_text);  
 		}
 		else if(count($details) == 7){  
@@ -206,12 +243,14 @@
 			//============   Tablename    ===================
 			$username = "lj".$phone;
 
+
+
 			//=================Write into database all the details=========================== 
 			$sql = "INSERT INTO subscribers (first_Name, last_Name, phone_Number, id_Number, email, business_Type, county, username, registration_Platform) 
 					VALUES ('$fname', '$sname', '$phone', '$id_number', '$email', '$business_type', '$county', '$username', 'MOBILE')";
 			if($conne->query($sql) == TRUE){
 				create_table($phone, $conne);
-				$message = "Thank you $fname for choosing Leja.\nYou are now our new member.";
+				$message = "Asante $fname Kwa kuchagua Leja.\n Wewe sasa ni mwanachama mpya.";
 				sendSMS(("+".$phone), $message);
 				echo "END ".$message;
 			}
@@ -247,18 +286,21 @@
 		}
     }
 
+
     function about(){
-    	$ussd_text="END <br>Leja is a USSD based inventory management system. Leja helps you keep records and make calculation of your business to ensure you convenience and profits.<br>Visit http://www.leja.co.ke to learn more";   
+    	$ussd_text="END <br>Leja ni mfumo wa usimamizi wa hesabu wa USSD. Leja inakusaidia kuweka kumbukumbu na kufanya hesabu ya biashara yako ili kuhakikisha urahisi na faida.<br>Tembelea http://www.leja.co.ke kujifunza zaidi";   
 		ussd_proceed($ussd_text);
     }
 
+	
+
     function update_purchases($details,$phone, $active_user, $conne){
 	    if (count($details)==1){  
-		    $ussd_text="CON \nEnter value of the goods you have bought today: ";  
+		    $ussd_text="CON \n Ingiza thamani ya bidhaa ulizonunulia leo: ";  
 		    ussd_proceed($ussd_text);  
 	    }  
 	    else if (count($details)==2){  
-		    $ussd_text="CON \nExtra expenditure on goods(eg. transportation)";  
+		    $ussd_text="CON \n Matumizi ya ziada kwa bidhaa (kwa mfano usafirishaji)";  
 		    ussd_proceed($ussd_text);  
 	    }  
 
@@ -274,6 +316,8 @@
 		    	$expenditure = NULL;
 		    } 
 
+
+
 		$sql = "SELECT  balance FROM $active_user ORDER BY id DESC LIMIT 1";
 		$result = $conne->query($sql);
 		if ($result->num_rows > 0) {
@@ -282,23 +326,27 @@
 			    $t_bal = $row["balance"] + $purchases + $expenditure;
 			} else {
 			    echo "0 results";
-			}
+			}   
+
+
 	      
 		    $sql = "INSERT INTO $active_user (purchases,expenditure,balance) VALUES ('$purchases','$expenditure','$t_bal')"; 
 
 		    if($conne->query($sql) == TRUE){
-		        $ussd_text="END \nYou have successfully recorded your purchases";  
+		        $ussd_text="END \n Umeandika manunuzi kwa mafanikio";  
 		    	ussd_proceed($ussd_text);
 		    }
 		    else{
 		        echo "error: ".$sql ."<br>" .$conne->error;
 		    }
+
 	    }  
 	}
 
 	function update_sales($details,$phone, $active_user, $conne){
 	     if (count($details)==1){  
-		    $ussd_text="CON \nEnter the total value of all goods sold today";  
+		    $ussd_text="CON \n 
+			Ingiza thamani ya jumla ya bidhaa zote kuuzwa leo";  
 		    ussd_proceed($ussd_text);  
 	    }   
 
@@ -310,6 +358,7 @@
 		    	$purchases = NULL;
 		    }
 
+
 		$sql = "SELECT  balance FROM $active_user ORDER BY id DESC LIMIT 1";
 		$result = $conne->query($sql);
 		if ($result->num_rows > 0) {
@@ -318,16 +367,20 @@
 			    $t_bal = $row["balance"] - $sales;
 			} else {
 			   // echo "0 results";
-			}	      
+			}   
+
+
+	      
 		    $sql = "INSERT INTO $active_user (sales,balance) VALUES ( '$sales','$t_bal')"; 
 
 		    if($conne->query($sql) == TRUE){
-		    	$ussd_text="END \nYou have successfully recorded your sales";  
+		    	$ussd_text="END \n Umeandika kumbukumbu yako kwa ufanisi";  
 		    	ussd_proceed($ussd_text);
 		    }
 		    else{
 		        echo "error: ".$sql ."<br>" .$conne->error;
 		    }
+
 	    }  
 	}
 
@@ -339,20 +392,22 @@
 				//calculations
 			    $t_bal = $row["balance"];
 			    if($t_bal > 0){
-			    	$ussd_text="END \nYou have a LOSS of: $t_bal. Sell more products and earn.";  
+			    	$ussd_text="END \nUna POSA ya: $t_bal. Uuza bidhaa zaidi na ulipate.";  
 		   			ussd_proceed($ussd_text);
 			    }elseif ($t_bal < 0){
-			    	$ussd_text="END \nYou have a PROFIT of: ".-1*$t_bal."Keep selling your goods and earn more";  
+			    	$ussd_text="END \nUna faida ya: ".-1*$t_bal."Endelea kuuza bidhaa zako ili upate zaidi";  
 		   			ussd_proceed($ussd_text);
 			    }else{
-			    	$ussd_text = "END \nYour profit and loss is balanced";
+			    	$ussd_text = "END \n faida na hasara yako ni sawa";
 			    	ussd_proceed($ussd_text);
 			    }
 			} else {
-				$ussd_text = "No results";
+				$ussd_text = "Hakuna matokeo";
 				ussd_proceed($ussd_text);
 		}
 	}
+
+
 
 	function statements($details,$phone, $active_user, $conne){
 		//Fetch email address of user
@@ -360,8 +415,8 @@
 
 		$result = $conne->query($sql);
 		if(!$result){
-			$ussd_text = "END Ooops!<br>
-						We cannot find your email. $phone
+			$ussd_text = "END Salale!<br>
+					Hatuwezi kupata barua pepe yako. $phone
 					";
 			ussd_proceed($ussd_text);
         }else{
@@ -373,7 +428,7 @@
 		$result =$conne->query($sql);
 		if(!$result)
 		{
-			$ussd_text = "END No results";
+			$ussd_text = "END Hakuna matokeo";
 			ussd_proceed($ussd_text);
 		}
 
@@ -381,6 +436,7 @@
 		$total_sales = 0;
 		$total_purchases = 0;
 		$total_expenditure = 0;
+
 		
 		//mail to:
 		$subject = "Statements for your business";
@@ -424,46 +480,19 @@
 		$message .="Total Expenditure;".$total_expenditure."<br>";
 		$message .="Net Total:".($total_sales-($total_expenditure + $total_purchases))."<br>"; 
 
+
 		// Set content-type when sending HTML email
 		$headers = "MIME-Version: 1.0" . "\r\n";
 		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 		// More headers
-		$headers .= 'From: Leja Business Statements <statements@leja.co.ke>' . "\n";
+		$headers .= 'From: Leja Business Statements <statements@leja.co.ke>' . "\r\n";
 		$headers .= "Return-Path: statements@leja.co.ke\n"; // Return path for errors
-		$headers .= "Content-type: text/html; charset=iso-8859-1";
 		$headers .= 'X-Mailer: PHP/' . phpversion();
 
 		$retval = mail($to,$subject,$message,$headers);
-		
-
-		//Also send a message
-		$clean_message = "LEJA MINI-STATEMENT"."\n";
-		
-		while ($row=mysqli_fetch_array($result)){		
-			$clean_message .= '
-					$row[_date]
-					$row[purchases]
-					$row[expenditure]
-					$row[sales]
-					$row[balance]
-					';
-           	$total_purchases += $row['purchases'];
-           	$total_expenditure += $row['expenditure'];
-           	$total_sales += $row['sales'];
-            $no++;
-		}//End while
-
-		$clean_message .="Total sales:".$total_sales."\n";
-		$clean_message .="Total purchases:".$total_purchases."\n";
-		$clean_message .="Total Expenditure;".$total_expenditure."\n";
-		$clean_message .="Net Total:".($total_sales-($total_expenditure + $total_purchases))."\n"; 
-
-		// Send sms
-		sendSMS($phone, $clean_message);
-
 		if($retval == TRUE){
-			$ussd_text = "END We have sent you an SMS with your statement and another copy to your email.";
+			$ussd_text = "END Your statement has been sent to your email.";
 			ussd_proceed($ussd_text);
 		}else{
 			$ussd_text = "END Ooops!<br>
@@ -472,16 +501,56 @@
 			ussd_proceed($ussd_text);
 		}
 
+		//Also send a message
+		$clean_message = "HERE IS YOUR BUSINESS STATEMENT"."\n";
+		$clean_message .= '<table style="font-family: verdana,arial,sans-serif;font-size:11px;color:#333333;border-width: 1px;border-color: #999999;border-collapse: collapse;">
+						<tr>
+							<th style="background:#b5cfd2;border-width: 1px;padding: 8px;border-style: solid;border-color: #999999;">Date</th>
+							<th style="background:#b5cfd2;border-width: 1px;padding: 8px;border-style: solid;border-color: #999999;">Purchases</th>
+							<th style="background:#b5cfd2;border-width: 1px;padding: 8px;border-style: solid;border-color: #999999;">Expenditure</th>
+							<th style="background:#b5cfd2;border-width: 1px;padding: 8px;border-style: solid;border-color: #999999;">Sales</th>
+							<th style="background:#b5cfd2;border-width: 1px;padding: 8px;border-style: solid;border-color: #999999;">Balance</th>
+						</tr>
+				';
+		
+		while ($row=mysqli_fetch_array($result)){		
+			$clean_message .= '
+				<tr>
+					<td style="background:#F5F5F5;border-width: 1px;padding: 8px;border-style: solid;border-color: #999999;">'.$row['_date'].'</td>
+		            <td style="background:#F5F5F5;border-width: 1px;padding: 8px;border-style: solid;border-color: #999999;">'.$row['purchases'].'</td>
+		            <td style="background:#F5F5F5;border-width: 1px;padding: 8px;border-style: solid;border-color: #999999;">'.$row['expenditure'].'</td>
+		            <td style="background:#F5F5F5;border-width: 1px;padding: 8px;border-style: solid;border-color: #999999;">'.$row['sales'].'</td>
+		            <td style="background:#F5F5F5;border-width: 1px;padding: 8px;border-style: solid;border-color: #999999;">'.$row['balance'].'</td>
+	           	</tr>';
+           	$total_purchases += $row['purchases'];
+           	$total_expenditure += $row['expenditure'];
+           	$total_sales += $row['sales'];
+            $no++;
+			}//End while
+
+		$clean_message .= "</table>";
+		$clean_message .= "</body></html>";
+
+		$clean_message .= "<br>";
+		$clean_message .= '<h3 style="color: lightgrey;">Grand totals</h3>';
+		$clean_message .="Total sales:".$total_sales."<br>";
+		$clean_message .="Total purchases:".$total_purchases."<br>";
+		$clean_message .="Total Expenditure;".$total_expenditure."<br>";
+		$clean_message .="Net Total:".($total_sales-($total_expenditure + $total_purchases))."<br>"; 
+		sendSMS('("+".$phone)', $clean_message);
+
 		$conne->close();
 	}
 
 	function sendSMS($recepient, $message){
 		require_once('AfricasTalkingGateway.php');
 
-		$username   = "brianokinyi";
-		$apikey     = "1c5223686d70be30004883b4be54a7c3fa29a063d0716ba2d7164ece2434e409";
+		$message = "Hello world!";
 
-		$gateway    = new AfricasTalkingGateway($username, $apikey);
+		$username   = "brianokinyi";
+		$apikey     = "72ee5e6f21378af8375c674f7d2c1a0ed0ff6d40a3e7932bb8032bd1661d2865";
+
+		$gateway    = new AfricasTalkingGateway($username, $apikey, "sandbox");
 
 		try 
 		{
@@ -491,6 +560,7 @@
 		{
 			//echo "Encountered an error while sending: ".$e->getMessage();
 		}
+
 	}
 
 	function getHelp() {
