94dd6ab85fc048199bb581bcb8bbf74dbbd987fe
mikespux <orengemichael12@gmail.com>
2018-03-30 15:57:21 +0300

    changing

--- index.php
+++ index.php
@@ -45,6 +45,7 @@
 	if ($result=mysqli_query($conn,$query)){
 		$row = $result->fetch_assoc();
 		if(mysqli_num_rows($result) > 0){
+			$isSwahili=1;
 			//View my account menu			
 			if ($level==0){
 				displaymenu($isSwahili);
