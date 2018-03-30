d9fee08ae6fcbe85afc8a3c8446dda5f9ae4d43a
mikespux <orengemichael12@gmail.com>
2018-03-30 10:44:49 +0300

    changing

--- index.php
+++ index.php
@@ -193,6 +193,8 @@
 					create_table_mamamboga($phone, $conne);
 				}else if($business_type == 'hotel'){
 					create_table_hotel($phone, $conne)
+				}else if($business_type == 'mtumba'){
+					create_table_mtumba($phone, $conne);
 				}else{
 					create_table($phone, $conne);
 				}
