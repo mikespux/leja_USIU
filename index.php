a0b8a6c7dd7d0436ec25c1772519507b725aeeb5
mikespux <orengemichael12@gmail.com>
2018-04-01 12:09:20 +0300

    updates

--- index.php
+++ index.php
@@ -448,7 +448,12 @@ function update_purchases_mamamboga($details,$phone,$active_user,$conne){
 				$ussd_text = "";
 			else
 				$ussd_text = "";
-		}
+		}else if (count($details)==3){
+			if($isSwahili == 1)
+				$ussd_text = "";
+			else
+				$ussd_text = "";
+		}else
 
 	}
 
